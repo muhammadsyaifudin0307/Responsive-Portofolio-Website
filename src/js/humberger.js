708e1f5e1820a52da84c8c90446f32a3851e9dc9
muhammadsyaifudin0307 <muhammadsyaifudin030703@gmail.com>
2024-12-21 14:22:50 +0700

    update again

--- src/js/humberger.js
+++ src/js/humberger.js
@@ -4,3 +4,9 @@ const navbar = document.querySelector(".nav-list");
 hamburgerMenu.addEventListener("click", () => {
   navbar.classList.toggle("active");
 });
+
+document.addEventListener("click", (event) => {
+  if (!navbar.contains(event.target) && !hamburgerMenu.contains(event.target)) {
+    navbar.classList.remove("active");
+  }
+});
